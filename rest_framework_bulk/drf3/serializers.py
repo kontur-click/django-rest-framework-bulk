0000bfd1e98490120539b5c2c52825713920eb47
Даниил Мясников <d.myasnikov@skbkontur.ru>
2019-06-18 16:50:26 +0500

    primary key instance fix

--- rest_framework_bulk/drf3/serializers.py
+++ rest_framework_bulk/drf3/serializers.py
@@ -3,6 +3,9 @@ import inspect
 
 from rest_framework.exceptions import ValidationError
 from rest_framework.serializers import ListSerializer
+from rest_framework.settings import api_settings
+from rest_framework.utils import html
+from rest_framework.fields import SkipField
 
 
 __all__ = [
@@ -68,3 +71,46 @@ class BulkListSerializer(ListSerializer):
             updated_objects.append(self.child.update(obj, obj_validated_data))
 
         return updated_objects
+
+    def to_internal_value(self, data):
+        """
+        List of dicts of native values <- List of dicts of primitive datatypes.
+        """
+        if html.is_html_input(data):
+            data = html.parse_html_list(data, default=[])
+
+        if not isinstance(data, list):
+            message = self.error_messages['not_a_list'].format(
+                input_type=type(data).__name__
+            )
+            raise ValidationError({
+                api_settings.NON_FIELD_ERRORS_KEY: [message]
+            }, code='not_a_list')
+
+        if not self.allow_empty and len(data) == 0:
+            if self.parent and self.partial:
+                raise SkipField()
+
+            message = self.error_messages['empty']
+            raise ValidationError({
+                api_settings.NON_FIELD_ERRORS_KEY: [message]
+            }, code='empty')
+
+        ret = []
+        errors = []
+
+        for item in data:
+            try:
+                self.child.instance = self.instance.get(id=item['id'])
+                self.child.initial_data = item
+                validated = self.child.run_validation(item)
+            except ValidationError as exc:
+                errors.append(exc.detail)
+            else:
+                ret.append(validated)
+                errors.append({})
+
+        if any(errors):
+            raise ValidationError(errors)
+
+        return ret
